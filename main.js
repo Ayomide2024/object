34829a279213b7dab21a2131d42519ee112d9bbf
J-boi <134131113+Ayomide2024@users.noreply.github.com>
2023-11-11 05:29:32 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,143 @@
+// // String Method
+ 
+// // CharAt mth
+// let bad = 'This is Nigeria'
+// // console.log(bad.charAt(4));
+
+// // Upper and LowerCase mth
+// // console.log(bad.toUpperCase());
+// // console.log(bad.toLocaleLowerCase());
+
+
+// // includes 
+// let sent = "I am coming home. Please make sure to cook yam";
+// // console.log(sent.includes('sure'));
+
+// // startsWith
+// // console.log(sent.startsWith('t'));
+
+// //endsWith
+// // console.log(sent.endsWith('y'));
+
+// //Slice
+// console.log(sent.slice(0, 4));
+
+// //SubString 
+
+// console.log(sent.substring(0, 4));
+
+
+
+
+
+
+
+
+
+
+
+
+
+let hhhjjdgdfhfjfjfj = 'dfghjklkjhgfdsdfghhhhhhhhhh'
+console.log(typeof(hhhjjdgdfhfjfjfj));
+
+
+
+
+
+
+
+let chai = 'wetin be this one like this'
+console.log(chai.charAt(7));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let omo = ' ooo aaaaa'
+console.log(omo.toUpperCase());
+console.log(omo.toLowerCase());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let nigeriaEeen = 'See aunty i dey busy. My own is that u should cok Yam'
+console.log(nigeriaEeen.includes('My'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let sapa = 'Sapa has stuck once again. Our eyes don see sege'
+console.log(sapa.startsWith('Sapa'));
+console.log(sapa.endsWith('sege'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let japa = 'In this Nigeria een people just dey japa.'
+console.log(japa.slice(20, 40));
+console.log(japa.substring(20, 40));
